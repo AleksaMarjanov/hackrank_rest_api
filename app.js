55dc693c0d330e03f0ab8b79e3b8f778e1e22010
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-09-30 20:20:31 -0500

    ..

--- app.js
+++ app.js
@@ -36,64 +36,29 @@
  *  https://jsonmock.hackerrank.com/api/transactions/search?userId=
  */
 
-
-
 const axios = require('axios');
-// import axios from "axios"
-
-// if(typeof window !== document ) {
-//     console.log('running in nodejs')
-// } else {
-//     console.log('running in Browser')
-//     const body = document.querySelector('.paragraph')
-// }
 
 const BASE_URL = 'https://jsonmock.hackerrank.com/api/transactions/search?userId='
 
 const getUserTransaction = async (uid, txnType, monthYear) => {
     // const result = []
-    const { data } = await axios.get(`${BASE_URL}`)
+    const { data } = await axios.get(`${BASE_URL}${uid}`)
     const fetchedData = data.data
     console.log(fetchedData); 
-    fetchedData.map(user => {
-            if(user.id === uid) {
-                let milliseconds = user.timestamp
-                let date = new Date(milliseconds).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
-                const monthYear = date.replace('/', '-')
-                console.log('here is the timestamp', monthYear)
-            }   
-        })     
+
+
+
+
+    // fetchedData.filter(user => {
+    //             user.monthYear;
+    //             let milliseconds = user.timestamp
+    //             let date = new Date(milliseconds).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
+    //             const monthYear = date.replace('/', '-')
+    //             console.log('here is the timestamp', monthYear)
+    //     })     
         // calculate average spending of the user in the Month year 
-
-
     }
 
-
-
-// async function main() {
-//     const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-//     const uid = parseInt(readLine().trim(), 10);
-
-//     const txnType = readLine();
-
-//     const monthYear = readLine();
-
-//     const result = await getUserTransaction(uid, txnType, monthYear);
-
-//     ws.write(result.join('\n') + '\n');
-
-//     ws.end();
-// }
 // test case
 getUserTransaction(4, 'debit', '02-2019')
 
-// try
-        // if(result.length === 0) return -1
-        // try {
-        //     const response = await axios.get(`${BASE_URL}`)    
-        //     let fetchedData = response.data
-        //     // console.log(fetchedData)
-        // } catch(error) {
-        //     console.log(error)
-        // } 
