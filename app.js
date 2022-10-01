fe887850d94020267c6b9d520e85f2b0790a16f8
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-10-01 17:55:14 -0500

    ..made this far

--- app.js
+++ app.js
@@ -16,23 +16,24 @@ const BASE_URL = 'https://jsonmock.hackerrank.com/api/transactions/search?userId
 
 const convertDate = (input) => {
     // converting input date from Milliseconds to Date
-    // let milliseconds = input;
-    // let date = new Date(milliseconds).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
-    // return convertedDate = date.replace('/', '-')
-    [m, y] = input.split("-");
-    return [y, m - 1]
+    let milliseconds = input;
+    let date = new Date(milliseconds).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
+    return convertedDate = date.replace('/', '-')
+    // [m, y] = input.split("-");
+    // return [y, m - 1]
 
 }
-// const extractObject = (obj, properties ) => {
-//     return properties.reduce((result, prop) => {
-//         if(obj.hasOwnProperty(prop)) {
-//             result[prop] = obj[prop]
-//         }
-//         return result
-//     }, {})
-// }
+const extractObject = (obj, properties ) => {
+    return properties.reduce((result, prop) => {
+        if(obj.hasOwnProperty(prop)) {
+            result[prop] = obj[prop]
+        }
+        return result
+    }, {})
+}
 
 const getUserTransaction = async (uid, txnType, monthYear) => {
+    let result = []
     let num = 0;
     const {data}  = await axios.get(`${BASE_URL}${uid}`)
     const totalPages = data.total_pages
@@ -40,19 +41,24 @@ const getUserTransaction = async (uid, txnType, monthYear) => {
         num++
         const dataForPages = await axios.get(`${BASE_URL}${uid}&page=${num}`)
         const allDataForUser = dataForPages.data.data
-        let time =  new Date(...convertDate(monthYear));
-        const convertedDate = time.getTime()
+        // let time =  new Date(...convertDate(monthYear));
+        // const convertedDate = time.getTime()
+        // console.log(convertedDate)
         // console.log(allDataForUser)
-        const extracted = allDataForUser.filter((obj) => {
-            return obj.txnType === 'debit';
-        })
-        console.log(extracted)
-
-        // const exactTime = extracted.find((mill) => mill.timestamp === convertedDate)
-        // console.log(exactTime)
-        // const extractedArray = allDataForUser.map((item) => {
-        //     return extractObject(item, ['txnType', 'timestamp' ])
+        // const extracted = allDataForUser.filter((obj) => {
+        //     return obj.txnType === 'debit';
         // })
+        const extractedArray = allDataForUser.map((item) => {
+            return extractObject(item, ['txnType', 'timestamp', 'amount' ])
+        })
+        // console.log(extractTransactionAndTimestamp)
+        const filteredArray = extractedArray.filter(item => item.txnType === txnType)
+        console.log(filteredArray)
+        // const exactTime = filteredArray.map((time) => {
+        //     return time.timestamp === convertedDate
+        // })
+            // console.log(exactTime)
+        // console.log(mappedArray)
         // // console.log(extractedArray)
         // const debitAndTimestamp = extractedArray.find(transaction => transaction.txnType === 'debit')
         // const result = debitAndTimestamp['debit']
