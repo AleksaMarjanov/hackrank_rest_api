b2224bb3b4774b75cdf599083dfa32779a40b9a3
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-10-01 17:24:13 -0500

    ..extracted just txnType === debit

--- app.js
+++ app.js
@@ -23,11 +23,18 @@ const convertDate = (input) => {
     return [y, m - 1]
 
 }
+// const extractObject = (obj, properties ) => {
+//     return properties.reduce((result, prop) => {
+//         if(obj.hasOwnProperty(prop)) {
+//             result[prop] = obj[prop]
+//         }
+//         return result
+//     }, {})
+// }
 
 const getUserTransaction = async (uid, txnType, monthYear) => {
     let num = 0;
     const {data}  = await axios.get(`${BASE_URL}${uid}`)
-    // const fetchedData = data.data
     const totalPages = data.total_pages
     for(let i = 0; i < totalPages; i++) {
         num++
@@ -35,19 +42,23 @@ const getUserTransaction = async (uid, txnType, monthYear) => {
         const allDataForUser = dataForPages.data.data
         let time =  new Date(...convertDate(monthYear));
         const convertedDate = time.getTime()
-        // convert user input for monthYear example: 02-2019 to milliseconds
-        let transaction = txnType;
-        console.log(convertedDate)
-        const monthYearRequested = allDataForUser.findIndex((date) => date.transaction == 'debit')
-        console.log(monthYearRequested)
-        // const filteredAllUserData = allDataForUser.filter(date => {
-        //     date.timestamp = convertedDate
+        // console.log(allDataForUser)
+        const extracted = allDataForUser.filter((obj) => {
+            return obj.txnType === 'debit';
+        })
+        console.log(extracted)
+
+        // const exactTime = extracted.find((mill) => mill.timestamp === convertedDate)
+        // console.log(exactTime)
+        // const extractedArray = allDataForUser.map((item) => {
+        //     return extractObject(item, ['txnType', 'timestamp' ])
         // })
-        // console.log(filteredAllUserData)
+        // // console.log(extractedArray)
+        // const debitAndTimestamp = extractedArray.find(transaction => transaction.txnType === 'debit')
+        // const result = debitAndTimestamp['debit']
+        // console.log(debitAndTimestamp)
         
-    }
-        // calculate average spending of the user in the Month year 
-    }
+    }}
 
 // test case
 getUserTransaction(4, 'debit', '02-2019')
