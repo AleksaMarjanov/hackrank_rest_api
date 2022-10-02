743ace14632b95c31102ba161a675da11083e0fc
MrWhite1338 <aleksa.marjanov@outlook.com>
2022-10-02 17:00:58 -0500

    solved

--- app.js
+++ app.js
@@ -11,75 +11,48 @@
  */
 
 const axios = require('axios');
-const _ = require('lodash')
+// const _ = require('lodash')
 
-const BASE_URL = 'https://jsonmock.hackerrank.com/api/transactions/search?userId='
+const BASE_URL = 'https://jsonmock.hackerrank.com/api/transactions/search?'
 
+// converting input date from Milliseconds to Date
 const convertDate = (input) => {
-    // converting input date from Milliseconds to Date
-    let milliseconds = input;
-    let date = new Date(milliseconds).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
-    return convertedDate = date.replace('/', '-')
-    // [m, y] = input.split("-");
-    // return [y, m - 1]
-}
-
-
-// Solution 2
-// helper function for extracting data from object in array
-// const extractObject = (obj, properties ) => {
-//     return properties.reduce((result, prop) => {
-//         if(obj.hasOwnProperty(prop)) {
-//             result[prop] = obj[prop]
-//         }
-//         return result
-//     }, {})
-// }a
-
+    let date = new Date(input).toLocaleDateString('en-us', {year: "numeric", month: "2-digit"})
+    return date.replace('/', '-')
+}    
 
 const getUserTransaction = async (uid, txnType, monthYear) => {
-    let result = []
-    let num = 0;
-    const {data}  = await axios.get(`${BASE_URL}${uid}`)
-    const totalPages = data.total_pages
-    for(let i = 0; i < totalPages; i++) {
-        num++
-        const dataForPages = await axios.get(`${BASE_URL}${uid}&page=${num}`)
-        const allDataForUserId = dataForPages.data.data
-        const mappedArray = allDataForUserId.map((item) => {
-            const timestamp = convertDate(item.timestamp)
-            const id = item.id
-            const amountNoComm = item.amount.replace(/,/, '');
-            const amountNo$ = amountNoComm.replace('$', '')
-            const amount = Number(amountNo$)
-            const txnType = item.txnType
-            return {id, amount, txnType, timestamp}
-        })
-        //  console.log(results)
-        // Solution 2
-        // console.log(mappedArray)
-        // const extractedArray = mappedArray.map((item) => {
-            //     return extractObject(item, ['txnType', 'timestamp', 'amount' ])
-            // })
-            const filteredArray = mappedArray.filter((item) => item.timestamp == monthYear && item.txnType == txnType)
-            console.log(filteredArray)
+    const result = [];
+    const totalPages = await axios.get(`${BASE_URL}userId=${uid}&txnType=${txnType}`).then((result) => result.data.total_pages);
 
-        // const averageMonthlySpending = filteredByTimestamp.reduce((total, item) => (total + item.amount) / filteredByTimestamp.length, 0)
-        // console.log(averageMonthlySpending)
-        // newArray.push(averageMonthlySpending)
-        // console.log(newArray)
+    for(let i = 1; i <= totalPages; i++) {
+        const results = await axios.get(`${BASE_URL}userId=${uid}&txnType=${txnType}&page=${i}`).then((result) => result.data);
+
+        results?.data?.map((item) => {
+            const resultObject = {
+                timestamp: convertDate(item.timestamp),
+                id: item.id,
+                txnType: item.txnType,
+                amount: Number(item.amount.replace(/,/, '').replace('$', ''))
+            }
+            result.push(resultObject);
+        })
         
-        // for ( let item of filteredByTimestamp) {
-        //     if(item.amount > average ) {
-        //         result.push(item.id)
-        //         console.log(result)
-        //     }
-        // }
     }
-    
+    console.log("REZ");
+    let finalResult = []
+    const filteredArray = result.filter((item) => item.timestamp == monthYear); 
+    const average = filteredArray.reduce((total, {amount}) => total + amount, 0) / filteredArray.length
+    console.log(average);
+
+        for(let item of filteredArray ) {
+            if( item.amount > average) {
+                finalResult.push(item.id)
+            }
+        }
+        console.log(finalResult)
 }
 
-
 // test case
-getUserTransaction(4, 'debit', '02-2019')
+getUserTransaction(4, 'debit', "02-2019")
 
